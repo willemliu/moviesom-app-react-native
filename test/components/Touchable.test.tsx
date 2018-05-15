626f62f159e6453143df111802ac5ea069803804
Willem Liu <willem_liu@hotmail.com>
2018-05-15 10:34:24 +0200

    Fixed test

--- test/components/Touchable.test.tsx
+++ test/components/Touchable.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, Platform} from 'react-native';
 import renderer from 'react-test-renderer';
 import Touchable from '../../src/components/Touchable';
 
@@ -7,7 +7,14 @@ const onPressHandler = () => {
     console.log('Pressed');
 };
 
-it('should render the correct Touchable component', () => {
+it('should render the Android Touchable component', () => {
+    Platform.OS = 'android';
+    const rendered = renderer.create(<Touchable onPress={onPressHandler}><Text>Touchable component</Text></Touchable>).toJSON();
+    expect(rendered).toMatchSnapshot();
+});
+
+it('should render the iOS Touchable component', () => {
+    Platform.OS = 'ios';
     const rendered = renderer.create(<Touchable onPress={onPressHandler}><Text>Touchable component</Text></Touchable>).toJSON();
     expect(rendered).toMatchSnapshot();
 });
